6adb8eef85aa1d9a41ad54987fc816c7394acd1b
Mike Nanto <mike@thecowboymechanic.com>
2021-01-17 03:05:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = NIL
   lowest_value = NIL
   name_hash.each do |k, v|
-    if lowest_value == nil || v > lowest_value
+    if lowest_value == nil || v < lowest_value
       
   
 
